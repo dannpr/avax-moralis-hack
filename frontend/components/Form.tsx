3f350f6289d6a8684cac2d244ba3240093829437
Pierregvx <pierregue75@gmail.com>
2021-12-25 19:33:20 +0100

    Update Form.tsx

--- frontend/components/Form.tsx
+++ frontend/components/Form.tsx
@@ -1,11 +1,22 @@
-
+function getElem(){
+   console.log('test');
+    var link = document.getElementById("form").textContent ;
+    console.log(link);
+    document.getElementById("j").innerHTML = link;
+    
+}
 const form =()=>
 {
-    return(<div className="post-input-container">
-                    <textarea  placeholder= "enter a link"></textarea>
+    return(<div id ="hj" className="post-input-container">
+                    <textarea id="link"  placeholder= "enter a link"></textarea>
                     <div className="add-post-links">
                         
                     </div>
-                <button>Submit</button>
+                
+                <form>
+                <input type="text" id="username" name="username" placeholder = "username"></input>
+                </form>
+                <button onClick={getElem}>Submit</button>
+                <span id = "j"></span>
                 </div>);}
 export default form
