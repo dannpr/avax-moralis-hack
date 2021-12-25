c571d1ca11dff219da1e483345221ed63c57c16f
Pierregvx <pierregue75@gmail.com>
2021-12-25 14:45:31 +0100

    connect moralis

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,13 +4,16 @@ import getLibrary from "../getLibrary";
 import "../styles/globals.css";
 import '../styles/account.css';
 import '../styles/form.css';
+import { MoralisProvider } from "react-moralis";
 
 
 function NextWeb3App({ Component, pageProps }: AppProps) {
   return (
+    <MoralisProvider appId={process.env.MORALIS_APPLICATION_ID } serverUrl={process.env.MORALIS_SERVER_ID}>
     <Web3ReactProvider getLibrary={getLibrary}>
       <Component {...pageProps} />
     </Web3ReactProvider>
+    </MoralisProvider>
   );
 }
 
